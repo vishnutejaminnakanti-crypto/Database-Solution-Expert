6c5a71a4a3ddccaa8d974212cc87da77cfcae809
Vishnu Teja <vishnutejaminnakanti@gmail.com>
2026-07-03 16:07:34 +0530

    Day 1 - Normalization

---
+++ Day 1/Normalization.sql
@@ -0,0 +1,12 @@
+create table customers (Member_id int, First_name varchar(50), Last_name varchar(50), Hobbies varchar(20));
+insert into customers values(101,'Jayson','Mark','cricket');
+insert into customers values(101,'Jayson','Mark','swimming');
+insert into customers values(101,'Jayson','Mark','football');
+insert into customers values(102,'Ram','Ganesh','Running');
+insert into customers values(102,'Ram','Ganesh','swimming');
+insert into customers values(102,'Ram','Ganesh','Music');
+insert into customers values(103,'Raj','Kishore','Dancing');
+insert into customers values(103,'Raj','Kishore','Singing');
+insert into customers values(103,'Raj','Kishore','Running');
+select * from customers;
+
